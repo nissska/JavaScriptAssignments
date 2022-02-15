0dc141b8ca107eace85e529b3013f932a4740492
nissska <niels.christian.cers@gmail.com>
2022-02-15 13:47:59 +0100

    second commit

--- Operators.js
+++ Operators.js
@@ -9,32 +9,46 @@ numOfDaysUntilWeekend -= 1;
 2.  Use console.log() to log the value right after the value of the variable has been changed. 
 3. Tell what it does 
 */
+let numOfDaysUntilWeekend = 2;
+console.log(numOfDaysUntilWeekend);
+numOfDaysUntilWeekend -= 1;
+console.log(numOfDaysUntilWeekend);
+//number of days before and after change of variable
 
 //assignemnt 2
 //Addition to numbers
-
+let l=1;
+l++;
 //assignemnt 3
 //Subtraction to numbers
+let i=1;
+i--;
 
 //assignemnt 4
 //write two sentences in two variables and connect them together 
-
+let firstWord='big';
+let secondWord=' cat';
+firstWord += secondWord;
+console.log(firstWord);
 //assignemnt 5
 //write two variables, one number and one string. add them together
-
+let firstWord='big';
+let secondWord='1';
+firstWord += secondWord;
+console.log(firstWord);
 //assignemnt 6
 //write the answears in the in a comment
 let r = 15;
 let s = 5;
-let t = r + s;
-let u = r - s;
-let v = t / u;
-let x = r * v;
-let y = s % v;
+let t = r + s; //20
+let u = r - s; //10
+let v = t / u; //2
+let x = r * v; //30
+let y = s % v; //0
 
 //assignemnt 7
 //Why does it not write out? 
-let r = 34;
+let r = 61;
 let c = 1;
 let x = r - 32 + c;
 let y = r++ * 4; 
