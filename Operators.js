8509c864fbb0d7a86dd347cb7da0381fb4817cda
nissska <niels.christian.cers@gmail.com>
2022-02-15 13:30:52 +0100

    1

---
+++ Operators.js
@@ -0,0 +1,68 @@
+'use strict';
+
+//assignemnt 1
+/*
+let numOfDaysUntilWeekend = 2;
+numOfDaysUntilWeekend -= 1;
+
+1. Use console.log() to log the value just after the variable has been declared
+2.  Use console.log() to log the value right after the value of the variable has been changed. 
+3. Tell what it does 
+*/
+
+//assignemnt 2
+//Addition to numbers
+
+//assignemnt 3
+//Subtraction to numbers
+
+//assignemnt 4
+//write two sentences in two variables and connect them together 
+
+//assignemnt 5
+//write two variables, one number and one string. add them together
+
+//assignemnt 6
+//write the answears in the in a comment
+let r = 15;
+let s = 5;
+let t = r + s;
+let u = r - s;
+let v = t / u;
+let x = r * v;
+let y = s % v;
+
+//assignemnt 7
+//Why does it not write out? 
+let r = 34;
+let c = 1;
+let x = r - 32 + c;
+let y = r++ * 4; 
+if(x == 30 || y == 2 ){
+ console.log("yeah you got it");
+}
+//change a number in a variable, so it print out the text
+
+
+//assignemnt 8
+//make two vaiables. One variable must contain a number and the other one must contain a prompt, 
+//where the user can enter a number. Match the two number and print out the answear
+//TIP: search prompt
+
+
+//assignment 9
+//write these numbers in the console and write the output. Explain you thing this happens
+    0.1 + 0.2;
+    0.1 + 0.2 === 0.3;
+    3  - 1
+    3  + 1
+    '3' - 1 
+    '3' + 1 
+    '222' - -'111'
+
+
+//assignemnt 9
+//Write a JavaScript program to determine whether a given year is a leap year 
+
+
+
